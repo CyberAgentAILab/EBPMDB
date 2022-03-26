55f97841111e7e626ef6b0cf4f6fcd51a59c0e74
daimoriwaki <daisuke.moriwaki@gmail.com>
2022-03-26 12:34:23 +0900

    fix for base path

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: process.env.GITHUB_PAGES ? '/EBPMDB' : '',
   // exportPathMaps: async function(defaultPathMap, { dev, dir, outDir, distDir, buildID }) {
   //   return {
   //     '/': { page: '/' },
