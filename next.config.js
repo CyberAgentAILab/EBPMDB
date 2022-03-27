0e61cdd587c19fd209636fce1a91e807a1244a4f
daimoriwaki <daisuke.moriwaki@gmail.com>
2022-03-27 12:27:15 +0900

    強制的にbasePathをいれる

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+//const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+const urlPrefix = "EBPMDB"
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
