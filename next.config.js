d9efd3756bb6e26ebf4d15b70f5c0c1739c04d21
daimoriwaki <daisuke.moriwaki@gmail.com>
2022-03-27 09:14:30 +0900

    modify config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
   // exportPathMaps: async function(defaultPathMap, { dev, dir, outDir, distDir, buildID }) {
   //   return {
   //     '/': { page: '/' },
