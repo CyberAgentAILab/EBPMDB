f86dfe23e5eeae257e86e7b6bab497cd9f446608
ken-g0 <goto_kentaro@cyberagent.co.jp>
2022-03-28 15:29:46 +0900

    update gitignore

--- node_modules/@mui/styled-engine/GlobalStyles/GlobalStyles.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Interpolation } from '@emotion/react';
-
-export interface GlobalStylesProps<Theme = {}> {
-  defaultTheme?: object;
-  styles: Interpolation<Theme>;
-}
-
-export default function GlobalStyles(props: GlobalStylesProps): React.ReactElement;
