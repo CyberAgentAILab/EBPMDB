d08421e104d7a19743c2dff96d8543bd3e32d21f
nosemasashi <nose_masashi@cyberagent.co.jp>
2022-05-31 17:27:24 +0900

    fix:headerを調整

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,21 +2,27 @@ import { Container, AppBar, Toolbar, Button, Box } from "@mui/material";
 import { GitHub } from "@mui/icons-material";
 
 export default function Navigation() {
-  return (
-    <AppBar position='relative' color='primary'>
-      <Container sx={{ m: 'auto' }}>
-        <Toolbar disableGutters>
-          <Button sx={{ color: '#ffffff'}} href={`/EBPMDB/`}>
-            EBPMDB
-          </Button>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Button href="https://github.com/CyberAgentAILab/EBPMDB/" target="_blank" rel="noopener">
-              <GitHub sx={{ color: '#ffffff' }} />
-            </Button>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  )
+	return (
+		<AppBar position="relative" color="primary">
+			<Container sx={{ m: "auto" }}>
+				<Toolbar
+					disableGutters
+					sx={{ display: "flex", justifyContent: "space-between" }}
+				>
+					<Button sx={{ color: "#ffffff" }} href={`/EBPMDB/`}>
+						EBPMDB
+					</Button>
+					<Box sx={{ flexGrow: 0 }}>
+						<Button
+							href="https://github.com/CyberAgentAILab/EBPMDB/"
+							target="_blank"
+							rel="noopener"
+						>
+							<GitHub sx={{ color: "#ffffff" }} />
+						</Button>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
