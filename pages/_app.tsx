e2d5c10fe3cff5f6067eb1cd0a6f0d8274af108a
ken-g0 <goto_kentaro@cyberagent.co.jp>
2022-06-01 19:24:42 +0900

    fix theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import theme from './theme/theme'
+import theme from '../theme/theme'
 import { ThemeProvider } from '@mui/material/styles'
 
 function MyApp({ Component, pageProps }: AppProps) {
