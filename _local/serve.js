f1be9536f9c33c825ebab9f0ddc54cea108e1890
ken-g0 <goto_kentaro@cyberagent.co.jp>
2022-11-14 13:04:10 +0900

    localでassetの参照できるよう修正

--- _local/serve.js
+++ _local/serve.js
@@ -3,10 +3,14 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
   const destUrl = request.url.replace('/EBPMDB', '')
+  const slugUrl = destUrl.replace('%5Bslug%5D', '[slug]')
   return handler(request, response, {
     public: "./out/",
     rewrites: [
-      { source: "/EBPMDB/_next/**", destination: `${destUrl}` }
+      { source: "/EBPMDB/_next/static/chunks/pages/doc/**.js", destination: `${slugUrl}`},
+      { source: "/EBPMDB/_next/**", destination: `${destUrl}` },
+      { source: "/EBPMDB/**.{png,jpg,svg}", destination: `${destUrl}` },
+      { source: "/EBPMDB/site.webmanifest", destination: `${destUrl}` },
     ],
     redirects: [
       { source: "/EBPMDB", destination: `${destUrl}` },
